8b2459acedde6f01a48cd713ba86a6b64ab93eaf
jackmheller <55434077+jackmheller@users.noreply.github.com>
2022-06-06 23:57:48 +0200

    add table

--- app.py
+++ app.py
@@ -1,22 +1,61 @@
-import dash
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  6 21:33:47 2022
+
+@author: medhahegde
+"""
+
+# import dash
 #import dash_html_components as html
 from dash import html
 #import dash_core_components as dcc
-from dash import dcc
+from dash import dcc,dash_table,Dash
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
+from dash import Input, Output
 
-from plotly.subplots import make_subplots
+
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 import pandas as pd
 import pycountry
+import ast
 
-from graphs import make_plot
+#data import for predictions
+pred_data = pd.read_csv('https://raw.githubusercontent.com/jackmheller/modernDataAnalytics/main/Data/pred_rankings.txt', sep="\t", header=None)
+true_data = pd.read_csv('https://raw.githubusercontent.com/jackmheller/modernDataAnalytics/main/Data/true_rankings.txt', sep="\t", header=None)
+predictions = ast.literal_eval(pred_data.iloc[0,0])
+true_rankings = ast.literal_eval(true_data.iloc[0,0])
 
+year = 2022
+pred = pd.DataFrame([range(1,6),predictions[year][:5],true_rankings[year][:5]]).T
+pred.columns = ['Rank','Predicted Top 5', 'True Top 5']
 
-app = dash.Dash(__name__,title='MDA Eurovision Interactive',external_stylesheets=[dbc.themes.BOOTSTRAP],serve_locally = False,suppress_callback_exceptions=True)
+def cond_formatting(pred):
+    cond_formatting_list=[]
+    for row_id in pred.index:
+        counter =0
+        if pred.iloc[row_id,1] in pred['True Top 5'].tolist():
+            query1 = {'if': {"row_index": row_id, 'column_id': 'Predicted Top 5'},
+      'backgroundColor': '#BCF7B3'}
+            counter+=1
+        if pred.iloc[row_id,2] in pred['Predicted Top 5'].tolist():
+            query2 = {'if': {"row_index": row_id, 'column_id': 'True Top 5'},
+      'backgroundColor': '#BCF7B3'}
+            counter+=1
+        if pred.iloc[row_id,1] == pred.iloc[row_id,2]:
+            query1 = {'if': {"row_index": row_id, 'column_id': 'Predicted Top 5'},
+      'backgroundColor': '#7FB277'}
+            query2 = {'if': {"row_index": row_id, 'column_id': 'True Top 5'},
+      'backgroundColor': '#7FB277'}
+            counter+=1
+        if counter>0:
+            cond_formatting_list.append(query1)
+            cond_formatting_list.append(query2)
+    return cond_formatting_list  
+
+app = Dash(__name__,title='MDA Eurovision Interactive',external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # add this for heroku
 server = app.server
@@ -26,7 +65,7 @@ noWeightData = pd.read_csv('https://raw.githubusercontent.com/jackmheller/modern
 weightedData = pd.read_csv('https://raw.githubusercontent.com/jackmheller/modernDataAnalytics/main/Data/GroupsScoreWeight.csv')
 data = noWeightData.merge(weightedData, on='name', suffixes = ("_noweight", "_weight"))
 
-df = pd.read_csv('https://raw.githubusercontent.com/jackmheller/modernDataAnalytics/main/yearBias.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/jackmheller/modernDataAnalytics/main/Data/yearBias.csv')
 
 countries = []
 for country in df['From country']:
@@ -76,6 +115,11 @@ fig3 = px.choropleth(locationmode="country names", scope="world", data_frame=dat
 fig3.update_layout(width=1500)
 
 
+card =dbc.Card([
+dcc.Dropdown([str(x) for x in list(predictions.keys())], '2022', id='demo-dropdown'),
+]
+)
+
 app.layout = dbc.Container(
     [
         html.Div(children=[html.H1(children='Mozambique Group World Visualizations'),
@@ -101,17 +145,65 @@ app.layout = dbc.Container(
 
         dbc.Row([dbc.Col(dcc.Graph(id="id_graph3",figure=fig3), md=10),],
                 align = "center"),
+        
+
+        html.Div(children=[html.H2(children='Prediction for Different Years')],
+        style = {'textAlign': 'center', 'color': 'black'}),
+        html.Hr(),
+        
+         dbc.Row([dbc.Col(card, md=4)],align='center',justify="center",),
+        
+        html.Br(),
+        html.Br(),
+        
+        html.Div(id='update-table',children=[dash_table.DataTable(
+        id='pred_table',
+        columns=[{"name": i, "id": i} 
+                  for i in pred.columns],
+        data=pred.to_dict('records'),
+        style_cell=dict(textAlign='center',font_family = 'arial',font_size = 12),
+        
+        style_header=dict(backgroundColor="#c7bebd",fontWeight='bold'),
+        style_data_conditional=cond_formatting(pred),  
+        fill_width=False)],style={'margin-left':'550px'}),
+          html.Br(),
+        html.Br()
+    
+        
     ],
     fluid=True,
 )
 
 @app.callback(
-    Output('id_title','children'),
-    Output('id_graph','figure'),
-    Output('id_graph','fig'),
+    Output("update-table", "children"),
+    [Input("demo-dropdown", "value")],
 )
-def update_chart(currency_value):
-    return
+def update_output(value):
+    
+    year = int(value)
+    pred = pd.DataFrame([range(1,6),predictions[year][:5],true_rankings[year][:5]]).T
+    pred.columns = ['Rank','Predicted Top 5', 'True Top 5']
+
+    return html.Div(
+        [
+            dash_table.DataTable(
+        id='pred_table',
+        columns=[{"name": i, "id": i} 
+                  for i in pred.columns],
+        data=pred.to_dict('records'),
+        style_cell=dict(textAlign='center',font_family = 'arial',font_size = 12),
+          style_cell_conditional=[
+        {'if': {'column_id': 'Rank'},
+          'width': '50px'},
+        {'if': {'column_id': 'Predicted Top 5'},
+          'width': '150px'},
+        {'if': {'column_id': 'True Top 5'},
+          'width': '150px'}],
+        style_header=dict(backgroundColor="#c7bebd",fontWeight='bold'),
+        style_data_conditional=cond_formatting(pred),  
+        fill_width=False)
+            
+        ])
 
 if __name__ == '__main__':
     app.run_server(debug=False)
